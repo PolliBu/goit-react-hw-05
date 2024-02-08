9c8bfced918f7c294f1df1bdbf7dbbde3812a529
PolliBu <8polli.bu@gmail.com>
2024-02-08 22:02:21 +0200

    Add pages

---
+++ src/Api.js
@@ -0,0 +1,21 @@
+// import axios from 'axios';
+
+// export const fetchData = async query => {
+//   const url =
+//     '<https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1>';
+
+//   const options = {
+//     headers: {
+//       Authorization: 'Bearer aa67eebd48ac511a9341a7e51636c98a',
+//     },
+//   };
+
+//   try {
+//     const response = await axios.get(url, options);
+//     console.log(response.data);
+//     return response.data;
+//   } catch (error) {
+//     console.error("This didn't work.");
+//     throw error;
+//   }
+// };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Products from '../pages/Products';
-import ProductDetails from '../pages/ProductDetails';
-import NotFound from '../pages/NotFound';
-import { AppBar } from './AppBar';
-import { Mission } from './Mission';
-import { Team } from './Team';
-import { Reviews } from './Reviews';
-import css from './App.module.css';
+import { Navbar } from './Navbar/Navbar';
+import HomePage from '../pages/HomePage';
+import MoviesPage from '../pages/MoviesPage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 export const App = () => {
   return (
-    <div className={css.container}>
-      <AppBar />
-
+    <div>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />}>
-          <Route path="mission" element={<Mission />} />
-          <Route path="team" element={<Team />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/movies" element={<MoviesPage />}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </div>
   );
