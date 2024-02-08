c5f74b8990f91fc176f33fbd87a4705ede56a61a
PolliBu <8polli.bu@gmail.com>
2024-02-08 23:56:08 +0200

    Add http popular

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,42 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function HomePage() {
-  return <div>Home</div>;
+  const [populars, setPopulars] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const API_KEY = 'aa67eebd48ac511a9341a7e51636c98a';
+      const url =
+        'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+      const options = {
+        params: {
+          api_key: API_KEY,
+        },
+      };
+      try {
+        const response = await axios.get(url, options);
+        setPopulars(response.data.results);
+      } catch (error) {
+        console.error("This didn't work.");
+        throw error;
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Trending today</h1>
+      {populars.length > 0 && (
+        <ul>
+          {populars.map(popular => (
+            <li key={popular.id}>
+              <p>{popular.title}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,3 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function MoviesPage() {
-  return <div>Movies</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const API_KEY = 'aa67eebd48ac511a9341a7e51636c98a';
+
+    const fetchData = async query => {
+      const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+
+      const options = {
+        params: {
+          api_key: API_KEY,
+        },
+      };
+
+      try {
+        const response = await axios.get(url, options);
+        setMovies(response.data);
+      } catch (error) {
+        console.error("This didn't work.");
+        throw error;
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <p>Movies</p>
+    </div>
+  );
 }
